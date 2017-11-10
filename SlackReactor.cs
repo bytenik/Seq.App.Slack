65959c0eb5217387ab78ebd892562f59802ae653
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-11-10 13:31:25 +1000

    Support Seq Alerts, including dashboard link and results links.

--- SlackReactor.cs
+++ SlackReactor.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Seq.Apps;
 using Seq.Apps.LogEvents;
 using System;
@@ -11,20 +10,15 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace Seq.Slack
+namespace Seq.App.Slack
 {
     [SeqApp("Slack Notifier", Description = "Sends messages matching a view to Slack.")]
     public class SlackReactor : Reactor, ISubscribeTo<LogEventData>
     {
         private static readonly Regex PlaceholdersRegex = new Regex("(\\[(?<key>[^\\[\\]]+?)(\\:(?<format>[^\\[\\]]+?))?\\])", RegexOptions.CultureInvariant | RegexOptions.Compiled);
+        private const uint AlertEventType = 0xA1E77000;
 
-        [SeqAppSetting(
-            DisplayName = "Seq Base URL",
-            HelpText = "Used for generating perma links to events in Slack messages.",
-        IsOptional = true)]
-        public string BaseUrl { get; set; }
         [SeqAppSetting(
             DisplayName = "Webhook URL",
             HelpText = "Slack labels this as \"Your Unique Webhook URL\".")]
@@ -37,10 +31,16 @@ namespace Seq.Slack
         public string Channel { get; set; }
 
         [SeqAppSetting(
-            DisplayName = "Username",
+            DisplayName = "App name",
             IsOptional = true,
-            HelpText = "The username that Seq uses when posting to Slack. If not specified, uses the Webhook default. Username can also be a PropertyKey in the format [PropertyKey].")]
+            HelpText = "The name that Seq uses when posting to Slack. If not specified, uses the Webhook default. The name can also be read from a property by using the format [PropertyName].")]
         public string Username { get; set; }
+
+        [SeqAppSetting(
+            DisplayName = "Seq base URL",
+            HelpText = "Used for generating links to events in Slack messages. The default is the value supplied by Seq.",
+            IsOptional = true)]
+        public string BaseUrl { get; set; }
 
         [SeqAppSetting(
             DisplayName = "Suppression time (minutes)",
@@ -49,17 +49,19 @@ namespace Seq.Slack
         public int SuppressionMinutes { get; set; } = 0;
 
         [SeqAppSetting(
-            DisplayName = "Exclude Properties",
+            DisplayName = "Exclude properties",
             IsOptional = true,
-            HelpText = "Should the event include the property information as attachments to the message. The default is to include")]
+            HelpText = "Should the event include the property information as attachments to the message. The default is to attach all properties.")]
         public bool ExcludePropertyInformation { get; set; }
 
         [SeqAppSetting(
-            HelpText = "The message template to use when writing the message to Slack. Refer to https://api.slack.com/docs/formatting for formatting options. Event property values can be added in the format [PropertyKey]. The default is \"[RenderedMessage]\"",
+            DisplayName = "Message",
+            HelpText = "The message to send to Slack. Refer to https://api.slack.com/docs/formatting for formatting options. Event property values can be added in the format [PropertyName]. The default is \"[RenderedMessage]\". Ignored for Seq Alerts.",
             IsOptional = true)]
         public string MessageTemplate { get; set; }
 
         [SeqAppSetting(
+            DisplayName = "Icon URL",
             HelpText = "The image to show in the room for the message. The default is https://getseq.net/images/nuget/seq.png",
             IsOptional = true)]
         public string IconUrl { get; set; }
@@ -111,13 +113,28 @@ namespace Seq.Slack
                 return;
             }
 
+            if (IsAlert(evt))
+            {
+                var resultsUrl = SafeGetProperty(evt, "ResultsUrl");
+                var resultsText = $"<{resultsUrl}|Explore detected results in Seq>";
+                var results = new
+                {
+                    color,
+                    text = resultsText
+                };
+                message.attachments.Add(results);
+
+                SendMessageToSlack(message);
+                return;
+            }
+
             var special = new
             {
                 color = color,
                 fields = new ArrayList { new { value = Enum.GetName(typeof(LogEventLevel), evt.Data.Level), title = "Level", @short = true } }
             };
-
             message.attachments.Add(special);
+
             foreach (var key in SpecialProperties)
             {
                 if (evt.Data.Properties == null || !evt.Data.Properties.ContainsKey(key)) continue;
@@ -174,13 +191,34 @@ namespace Seq.Slack
 
         private string GenerateMessageText(Event<LogEventData> evt)
         {
-            if (string.IsNullOrWhiteSpace(MessageTemplate))
-                MessageTemplate = "[RenderedMessage]";
-
-            var messageTemplateToUse = MessageTemplate;
-
             var seqUrl = string.IsNullOrWhiteSpace(BaseUrl) ? Host.ListenUris.FirstOrDefault() : BaseUrl;
-            return $"{SubstitutePlaceholders(messageTemplateToUse, evt)} (<{seqUrl}/#/events?filter=@Id%20%3D%3D%20%22{evt.Id}%22&show=expanded|View on Seq>)";
+
+            if (IsAlert(evt))
+            {
+                var dashboardUrl = SafeGetProperty(evt, "DashboardUrl");
+                var condition = SafeGetProperty(evt, "Condition", raw: true);
+                var dashboardTitle = SafeGetProperty(evt, "DashboardTitle");
+                var chartTitle = SafeGetProperty(evt, "ChartTitle");
+                return $"Alert condition `{condition}` detected on <{dashboardUrl}|{dashboardTitle}/{chartTitle}>.";
+            }
+
+            var messageTemplateToUse = string.IsNullOrWhiteSpace(MessageTemplate) ? "[RenderedMessage]" : MessageTemplate;
+            return $"{SubstitutePlaceholders(messageTemplateToUse, evt)} (<{seqUrl}/#/events?filter=@Id%20%3D%3D%20%22{evt.Id}%22&show=expanded|View this event in Seq>)";
+        }
+
+        private static bool IsAlert(Event<LogEventData> evt)
+        {
+            return evt.EventType == AlertEventType;
+        }
+
+        private static string SafeGetProperty(Event<LogEventData> evt, string propertyName, bool raw = false)
+        {
+            if (evt.Data.Properties.TryGetValue(propertyName, out var value))
+            {
+                if (value == null) return "`null`";
+                return raw ? value.ToString() : SlackMessage.Escape(value.ToString());
+            }
+            return "";
         }
 
         private void SendMessageToSlack(object message)
@@ -217,7 +255,7 @@ namespace Seq.Slack
 
         private string FormatValue(object value, string format)
         {
-            var rawValue = value?.ToString() ?? "(Null)";
+            var rawValue = value?.ToString() ?? "`null`";
 
             if (string.IsNullOrWhiteSpace(format))
                 return rawValue;
@@ -228,7 +266,7 @@ namespace Seq.Slack
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "Could not format Slack message: {value} {format}", value, format);
+                Log.Error(ex, "Could not format Slack message: {Value} {Format}", value, format);
             }
 
             return rawValue;
